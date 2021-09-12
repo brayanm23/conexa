31d40c39a51d410b44a59c6d929f603b94460897
Brayan Mendoza <brayan.mendoza@kavak.com>
2021-09-12 18:17:32 -0300

    add rxjava in networking

--- networking/src/main/java/com/conexa/networking/RetrofitBuilder.kt
+++ networking/src/main/java/com/conexa/networking/RetrofitBuilder.kt
@@ -1,6 +1,7 @@
 package com.conexa.networking
 
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
@@ -11,6 +12,7 @@ object RetrofitBuilder {
     private fun initRetrofit(baseUrl: String): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
